d9117661c73494a19c80e37cf24d4cf2746b212c
TsarGun <mzubrit@ilstu.edu>
2020-04-25 19:35:08 -0500

    Finished researching Vue

---
+++ client/scr/VueApp.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="vue-app">
+        <Header />
+            <main>
+                <router-view />
+            </main>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export default {
+  name: 'VueApp',
+  data () {
+    return {}
+  },
+  components: {
+    Footer,
+    Header
+  },
+  mounted () {
+    // TODO: Maybe cookie to see user preference for color scheme
+    document.body.classList.add('pal-solar-lt')
+  }
+}
+</script>
+
+<style>
+@import './assets/css/layouts.css';
+@import './assets/css/palettes.css';
+</style>
